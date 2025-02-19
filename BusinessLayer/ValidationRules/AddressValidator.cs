2a707328c1d41b948964bcdfbe7ae6e865d542f2
Ahmet Koçyiğit <kocyigittahmet@gmail.com>
2025-02-19 22:44:56 +0300

    Add files via upload

---
+++ BusinessLayer/ValidationRules/AddressValidator.cs
@@ -0,0 +1,26 @@
+﻿using EntityLayer.Concrete;
+using FluentValidation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessLayer.ValidationRules
+{
+    public class AddressValidator : AbstractValidator<Address>
+    {
+        public AddressValidator()
+        {
+            RuleFor(x => x.Description1).NotEmpty().WithMessage("Açıklama 1 Boş Geçilemez");
+            RuleFor(x => x.Description1).MaximumLength(25).WithMessage("Açıklama 25 Karakter Olmalıdır");
+            RuleFor(x => x.Description2).NotEmpty().WithMessage("Açıklama 2 Boş Geçilemez");
+            RuleFor(x => x.Description1).MaximumLength(25).WithMessage("Açıklama 25 Karakter Olmalıdır");
+            RuleFor(x => x.Description3).NotEmpty().WithMessage("Açıklama 3 Boş Geçilemez");
+            RuleFor(x => x.Description1).MaximumLength(25).WithMessage("Açıklama 25 Karakter Olmalıdır");
+            RuleFor(x => x.Description4).NotEmpty().WithMessage("Açıklama 4 Boş Geçilemez");
+            RuleFor(x => x.Description1).MaximumLength(25).WithMessage("Açıklama 25 Karakter Olmalıdır");
+            RuleFor(x => x.Mapinfo).NotEmpty().WithMessage("Harita Bilgisi Boş Geçilemez");
+        }
+    }
+}
